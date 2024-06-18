6e5abd4b56ea0824c1ada2f07dd51c9e2977f0ef
JJJHolscher <jochem.holscher@gmail.com>
2024-06-18 14:27:28 +0200

    little generations bugs

--- features2image_diffusion/unet.py
+++ features2image_diffusion/unet.py
@@ -296,7 +296,7 @@ class DDPM(nn.Module):
             guide_w: how much the context influences the generations
         """
 
-        batch_size = n_sample * len(context)
+        batch_size = n_sample # * len(context)
         # sample initial noise x_T ~ N(0, 1)
         x_i = torch.randn(batch_size, *size).to(device)
         # context for us just cycles throught the mnist labels
--- features2image_diffusion/vis.py
+++ features2image_diffusion/vis.py
@@ -3,48 +3,8 @@
 
 from jaxtyping import Float
 from jo3mnist.vis import to_img
-import torch
 import matplotlib.pyplot as plt
 import numpy as np
-
-
-def old_evaluation_section():
-    n_sample = 4*n_classes
-    for w_i, w in enumerate(ws_test):
-        x_gen, x_gen_store = ddpm.sample(n_sample, (1, 28, 28), device, guide_w=w)
-
-        # append some real images at bottom, order by class also
-        x_real = torch.Tensor(x_gen.shape).to(device)
-        for k in range(n_classes):
-            for j in range(int(n_sample/n_classes)):
-                try:
-                    idx = torch.squeeze((c == k).nonzero())[j]
-                except:
-                    idx = 0
-                x_real[k+(j*n_classes)] = x[idx]
-
-        x_all = torch.cat([x_gen, x_real])
-        grid = make_grid(x_all*-1 + 1, nrow=10)
-        save_image(grid, save_dir + f"image_ep{ep}_w{w}.png")
-        print('saved image at ' + save_dir + f"image_ep{ep}_w{w}.png")
-
-        if ep % 5 == 0 or ep == int(n_epoch-1):
-            # gif of images evolving over time, based on x_gen_store
-            fig, axs = plt.subplots(nrows=int(n_sample/n_classes), ncols=n_classes,sharex=True,sharey=True,figsize=(8,3))
-            def animate_diff(i, x_gen_store):
-                print(f'gif animating frame {i} of {x_gen_store.shape[0]}', end='\r')
-                plots = []
-                for row in range(int(n_sample/n_classes)):
-                    for col in range(n_classes):
-                        axs[row, col].clear()
-                        axs[row, col].set_xticks([])
-                        axs[row, col].set_yticks([])
-                        # plots.append(axs[row, col].imshow(x_gen_store[i,(row*n_classes)+col,0],cmap='gray'))
-                        plots.append(axs[row, col].imshow(-x_gen_store[i,(row*n_classes)+col,0],cmap='gray',vmin=(-x_gen_store[i]).min(), vmax=(-x_gen_store[i]).max()))
-                return plots
-            ani = FuncAnimation(fig, animate_diff, fargs=[x_gen_store],  interval=200, blit=False, repeat=True, frames=x_gen_store.shape[0])    
-            ani.save(save_dir + f"gif_ep{ep}_w{w}.gif", dpi=100, writer=PillowWriter(fps=5))
-            print('saved image at ' + save_dir + f"gif_ep{ep}_w{w}.gif")
 
 
 def tabulate_generations(
@@ -67,7 +27,7 @@ def tabulate_generations(
         c = i % cols
         if len(generation.shape) == 3:
             generation = (generation + 2.1008) / (2.64 + 2.1008)
-            axs[r, c].imshow(generation.permute(1, 2, 0))
+            axs[r, c].imshow(np.transpose(generation, (1, 2, 0)))
         else:
             axs[r, c].imshow(to_img(generation))
         axs[r, c].set_axis_off()
